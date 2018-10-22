5800a196fbdcfaec72cdc18886bce86eade9b5b4
Jim Gough <jpgough@gmail.com>
2018-10-22 19:48:26 +0100

    Initial Commit

---
+++ src/main/java/com/oreilly/message/MessageController.java
@@ -0,0 +1,20 @@
+package com.oreilly.message;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class MessageController {
+
+    @RequestMapping(path = "/message", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public String message() {
+        return new String("I am version 3");
+    }
+
+
+}
